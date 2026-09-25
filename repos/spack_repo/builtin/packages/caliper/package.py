ce5dc9dfd6b0f31384a0d7819323c71cb9fd1b6d
Tom Payerle <payerle@umd.edu>
2026-09-25 16:27:54 -0400

    caliper: Add version restriction on variorum
    variorum@0.8 replaced 'variorum_get_node_power_json' with
    'variorum_get_power_json', but caliper@:2.15a(lastest)
    refers to the former.
    
    So impose a version restriction in variorum dependency

--- repos/spack_repo/builtin/packages/caliper/package.py
+++ repos/spack_repo/builtin/packages/caliper/package.py
@@ -88,6 +88,7 @@ class Caliper(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("unwind@1.2:1", when="+libunwind")
     depends_on("elfutils", when="+libdw")
     depends_on("variorum", when="+variorum")
+    depends_on("variorum@:0.7", when="+variorum")
     depends_on("intel-oneapi-vtune", when="+vtune")
 
     depends_on("cmake", type="build")
